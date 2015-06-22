b51af0b45fc26a3f588690df34708056df1179e1
Pouria <Pouria@192.168.0.2>
2015-06-22 22:15:21 +1000

    SECOND COMMIT

---
+++ DRS/DRS-ejb/src/java/net/pba/drs/DRS.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.pba.drs;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Pouria
+ */
+@Stateless
+@LocalBean
+public class DRS {
+
+    
+}
